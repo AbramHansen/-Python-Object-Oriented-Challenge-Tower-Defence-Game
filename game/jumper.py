bf70318ccbb4d4b7b92c44f2e8d8d28bac222e4f
Samson Idoko <Samsonidoko3@byui.edu>
2021-03-09 20:09:06 -0700

    jumper class

---
+++ game/jumper.py
@@ -0,0 +1,13 @@
+class Jumper:
+    
+    def __init__ (self):
+        self.guesses = []
+        self.parachutePieces = 4
+    def getGuesses(self):
+        return self.guesses
+    def addGuess(self, guess):
+        self.guesses.append(guess)
+    def getLastGuess(self):
+        self.guesses[-1]
+    def cutParachute(self):
+        self.parachutePieces -= 1
